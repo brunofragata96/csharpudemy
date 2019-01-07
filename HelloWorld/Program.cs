88e248c559b26f94f2c8b358f32d1c340c18b51c
Bruno Fragata <brunofragata7@gmail.com>
2019-01-07 14:25:43 +0000

    first-real-commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello my name is Bruno and i'm trying my best not to show that i have absolutely no idea of what i'm doing here.");
+        }
+    }
+}
